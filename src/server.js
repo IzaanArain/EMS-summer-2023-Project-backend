1952633637026457f389e590f46d45d2687dfc58
IzaanArain <izaansaquib@gmail.com>
2023-07-21 14:52:54 +0500

    vercel deployment

--- src/server.js
+++ src/server.js
@@ -5,18 +5,23 @@ const UserRoutes = require("./routes/UserRoutes");
 const ConnectDB = require("./config/DbConnection");
 const colors = require("colors");
 const ErrorHandler = require("./middleware/ErrorHandler");
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 
 ConnectDB();
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin:["https://ems-frontend-izaan.vercel.app","http://localhost:5173"],
+  methods:["GET","POST","PUT","DELETE","PATCH"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req,res,next)=>{
-  console.log(`${req.method} ${req.path}`.magenta.italic)
-  next()
-})
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`.magenta.italic);
+  next();
+});
 app.use("/api/events/", EventRoutes);
 app.use("/api/users", UserRoutes);
 app.use(ErrorHandler);
@@ -34,5 +39,7 @@ PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server connected at Port http://localhost:${PORT}/api/events/`);
-  console.log(`Server connected at Port http://localhost:${PORT}/api/users/login`);
+  console.log(
+    `Server connected at Port http://localhost:${PORT}/api/users/login`
+  );
 });
